4135231be29ccb8ba1cc9623a8225322189d0155
lvying603 <ylu720@usc.edu>
2017-05-05 20:29:55 -0700

    Add functions for generating quadralaterals and kml file for NII drone videos.

--- drone-metadata-generator/area-comparison.cpp
+++ drone-metadata-generator/area-comparison.cpp
@@ -4,7 +4,7 @@
 #include <iomanip>
 #include <algorithm>
 #include <random>
-#include <cmath> 
+#include <cmath>
 
 #include "drone.h"
 
@@ -25,7 +25,7 @@ int main(int argc, char* argv[])
 		std::cout << "Couldn't open " << data_fname << std::endl;
 		return -1;
 	}
-	
+
 	std::string out_fname(argv[2]);
 	ofstream out_file;
 	out_file.open(out_fname);
@@ -33,8 +33,8 @@ int main(int argc, char* argv[])
 		std::cout << "Couldn't open " << out_fname << std::endl;
 		return -1;
 	}
-	
-	
+
+
 	std::vector<REALTYPE> quadrilateral(8);
 	REALTYPE timestamp;
 	REALTYPE azimuth;
@@ -42,46 +42,46 @@ int main(int argc, char* argv[])
 	std::string line;
 	int linenum = 0;
 	REALTYPE avg_mbr_area=0.0, avg_quadrilateral_area=0.0, avg_area_diff=0.0;
-	
+
 	while(!data_file.eof())
 	{
 		std::getline(data_file, line);
 		std::istringstream iss(line);
-		iss >> quadrilateral[0] >> comma >> quadrilateral[1] >> comma 
+		iss >> quadrilateral[0] >> comma >> quadrilateral[1] >> comma
 		    >> quadrilateral[2] >> comma >> quadrilateral[3] >> comma
 			>> quadrilateral[4] >> comma >> quadrilateral[5] >> comma
 			>> quadrilateral[6] >> comma >> quadrilateral[7] >> comma
 			>> azimuth >> comma
 			>> timestamp;
-		
+
 		// Calculate the area of the quadrilateral.
 		REALTYPE quadrilateral_area = calc_quadrilateral_area(quadrilateral);
 		avg_quadrilateral_area += quadrilateral_area;
-		
+
 		// Calculate the area of the MBR of the quadrilateral.
 		REALTYPE mbr_area = calc_quadrilateral_mbr_area(quadrilateral);
 		avg_mbr_area += mbr_area;
-		
+
 		REALTYPE area_diff = mbr_area - quadrilateral_area;
 		avg_area_diff += area_diff;
-		
+
 		out_file << quadrilateral_area << ','
 		         << mbr_area << ','
 				 << azimuth << ','
 		         << area_diff/mbr_area*100.0 << '\n';
 		linenum ++;
 	}
-	
+
 	avg_quadrilateral_area /= linenum;
 	avg_mbr_area /= linenum;
 	avg_area_diff /= linenum;
-	
+
 	std::cout << "avg_quadrilateral_area: " << avg_quadrilateral_area <<'\n'
 	          << "avg_mbr_area: " << avg_mbr_area << '\n'
 			  << "avg_area_diff: " << avg_area_diff << '\n'
 			  << "dead space ratio: " << avg_area_diff/avg_mbr_area*100.0 << '\n';*/
-			  
-	
+
+
 	/***
 	 * Analyze the deadspace ratio affected by azimuth, pitch and roll angles.
 	 */
@@ -92,50 +92,58 @@ int main(int argc, char* argv[])
 		std::cout << "Couldn't open " << out_fname << std::endl;
 		return -1;
 	}
-	
-	REALTYPE lat = 34.020551;
-	REALTYPE lng = -118.290452;
-	REALTYPE hgt = 1; //camera height (distance from ground, kilometer)
+
+	REALTYPE lat = 34.020578; //34.020551;
+	REALTYPE lng = -118.285416; //-118.290452;
+	REALTYPE hgt = 0.2; //camera height (distance from ground, kilometer)
 	REALTYPE azimuth = 0.0; //w.r.t. the north, in degree.
 	REALTYPE pitch = 0.0; //in degree
 	REALTYPE roll = 0.0; //in degree
-	REALTYPE viewable_angle = 80.0; //camera viewable angle (in degree)
-	
-	
+	REALTYPE viewable_angle = 100.0; //camera viewable angle (in degree)
+
+
 	for(azimuth = 0.0; azimuth<360; azimuth = azimuth+30)
-		for(roll = 0.0; roll<90.0; roll = roll+10)
-			for(pitch=0.0; pitch<90.0; pitch = pitch+10)
+		for(roll = 0.0; roll<30.0; roll = roll+17)
+			for(pitch=0.0; pitch<30.0; pitch = pitch+17)
 	{
-		Drone drone(lat, lng, hgt, azimuth, pitch, roll, viewable_angle); 
+		Drone drone(lat, lng, hgt, azimuth, pitch, roll, viewable_angle);
 		drone.calc_coverage();
-		
+
 		// Calculate the area of the quadrilateral.
-		REALTYPE quadrilateral_area = drone.calc_quadrilateral_area();
-		
+		//REALTYPE quadrilateral_area = drone.calc_quadrilateral_area();
+
 		// Calculate the area of the MBR of the quadrilateral.
-		std::vector<REALTYPE> MBR(4); 
+		/*std::vector<REALTYPE> MBR(4);
 		REALTYPE mbr_area = drone.calc_quadrilateral_mbr_area(MBR);
-		REALTYPE area_diff = mbr_area - quadrilateral_area;
-		
+		REALTYPE area_diff = mbr_area - quadrilateral_area;*/
+
 		// Print information.
 		char seprator = ',';
-		out_file << azimuth << seprator << roll << seprator << pitch << seprator
+		/*out_file << azimuth << seprator << roll << seprator << pitch << seprator
 		         << quadrilateral_area << seprator
 		         << mbr_area << seprator
 				 << azimuth << seprator
-		         << area_diff/mbr_area*100.0 << seprator;
+		         << area_diff/mbr_area*100.0 << seprator;*/
+		out_file << azimuth << seprator << roll << seprator << pitch << seprator;
 		
 		for(size_t qi=0; qi+1<drone.quadrilateral.size(); qi = qi + 2)
-			out_file << std::fixed << std::setprecision(6) 
-		             << "{lat: " << drone.quadrilateral[qi] << ", lng: " << drone.quadrilateral[qi+1] << "}, ";
-	
-	    for(auto r : MBR)
-			out_file << std::fixed << std::setprecision(6) << r << seprator;
-		
+		{
+			/*out_file << std::fixed << std::setprecision(6)
+		             << "{lat: " << drone.quadrilateral[qi] << ", lng: " << drone.quadrilateral[qi+1] << "}, ";*/
+			 out_file << std::fixed << std::setprecision(6)
+ 		             << drone.quadrilateral[qi+1] << "," << drone.quadrilateral[qi] << ", ";
+		}
+
+        //print middle point.
+		out_file << std::fixed << std::setprecision(6)
+		         << lng << "," << lat << ",";
+
+	    /*for(auto r : MBR)
+			out_file << std::fixed << std::setprecision(6) << r << seprator;*/
+
 		out_file << '\n';
 	}
-	
-	out_file.close();	  
+
+	out_file.close();
 	return 0;
 }
-	
--- drone-metadata-generator/calc-quatrilaterals.cpp
+++ drone-metadata-generator/calc-quatrilaterals.cpp
@@ -16,16 +16,16 @@ int main(int argc, char* argv[])
 	REALTYPE pitch = 0.0; //in degree
 	REALTYPE roll = 0.0; //in degree
 	REALTYPE viewable_angle = 0.0; //camera viewable angle (in degree)
-	if(argc > 3)
+	if(argc > 1 and argv[1]==std::string("process-one-fov"))
 	{
 		/****** One FOV ******/
-		lat = atof(argv[1]);
-		lng = atof(argv[2]);
-		hgt = atof(argv[3]); //camera height (distance from ground, kilometer)
-		azimuth = atof(argv[4]); //w.r.t. the north, in degree.
-		pitch = atof(argv[5]); //in degree
-		roll = atof(argv[6]); //in degree
-		viewable_angle = atof(argv[7]); //camera viewable angle (in degree)
+		lat = atof(argv[2]);
+		lng = atof(argv[3]);
+		hgt = atof(argv[4]); //camera height (distance from ground, kilometer)
+		azimuth = atof(argv[5]); //w.r.t. the north, in degree.
+		pitch = atof(argv[6]); //in degree
+		roll = atof(argv[7]); //in degree
+		viewable_angle = atof(argv[8]); //camera viewable angle (in degree)
 		Drone drone(lat, lng, hgt, azimuth, pitch, roll, viewable_angle); 
 		
 		drone.calc_coverage();
@@ -37,10 +37,10 @@ int main(int argc, char* argv[])
 				  << "{lat: " << drone.quadrilateral[6] << ", lng: " << drone.quadrilateral[7] << "}"
 				  << std::endl;
 	}
-	else
+	else if(argc > 1 and argv[1]==std::string("atlantik-drone-video-file"))
 	{
-		/****** Process a set of FOVs in a file *******/
-		std::string data_fname(argv[1]);
+		/****** Process a set of FOVs in a file (atlantik-drone-video-file) *******/
+		std::string data_fname(argv[2]);
 		ifstream data_file;
 		data_file.open(data_fname);
 	    if (!data_file.is_open()) {
@@ -48,7 +48,7 @@ int main(int argc, char* argv[])
 		    return -1;
 	    }
 		
-		std::string out_fname(argv[2]);
+		std::string out_fname(argv[3]);
 		ofstream out_file;
 		out_file.open(out_fname, std::ofstream::out | std::ofstream::app);
 		if (!out_file.is_open()) {
@@ -82,6 +82,152 @@ int main(int argc, char* argv[])
 					 << timestr << "\n";
 		}
 	}
+	else if(argc > 1 and argv[1]==std::string("nii-drone-video-file"))
+	{
+		/****** Process a set of FOVs in a file (nii-drone-video-file) *******/
+		std::string data_fname(argv[2]);
+		ifstream data_file;
+		data_file.open(data_fname);
+	    if (!data_file.is_open()) {
+	    	std::cout << "Couldn't open " << data_fname << std::endl;
+		    return -1;
+	    }
+		
+		std::string out_fname(argv[3]);
+		ofstream out_file;
+		out_file.open(out_fname, std::ofstream::out | std::ofstream::app);
+		if (!out_file.is_open()) {
+	    	std::cout << "Couldn't open " << out_fname << std::endl;
+		    return -1;
+	    }
+		
+		viewable_angle = 40.0;
+		std::string line;
+		std::getline(data_file, line);
+		char comma;
+		std::string timestr;
+		while(!data_file.eof())
+		{
+			std::getline(data_file, line);
+			std::istringstream iss(line);
+		    iss>>lat>>comma>>lng>>comma>>hgt>>comma>>azimuth>>comma>>pitch>>comma>>roll>>comma>>timestr;
+			hgt = hgt / 1000.0; //meter -> kilometer
+			pitch = pitch + 90.0;
+			roll = 0.0;
+			Drone drone(lat, lng, hgt, azimuth, pitch, roll, viewable_angle); 
+			drone.calc_coverage();
+			//print quadrilateral
+			out_file << std::fixed << std::setprecision(6)
+					 << drone.quadrilateral[1] << "," << drone.quadrilateral[0] << ",0,"
+					 << drone.quadrilateral[3] << "," << drone.quadrilateral[2] << ",0,"
+					 << drone.quadrilateral[5] << "," << drone.quadrilateral[4] << ",0,"
+					 << drone.quadrilateral[7] << "," << drone.quadrilateral[6] << ",0,"
+					 << lng << "," << lat << "," << hgt*1000.0 << "\n";
+		}
+	}
+	else if(argc > 1 and argv[1]==std::string("nii-generate-kml"))
+	{
+		/****** Process a set of FOVs in a file (nii-drone-video-file) and generate kml file*******/
+		std::string data_fname(argv[2]);
+		ifstream data_file;
+		data_file.open(data_fname);
+	    if (!data_file.is_open()) {
+	    	std::cout << "Couldn't open " << data_fname << std::endl;
+		    return -1;
+	    }
+		
+		std::string out_fname(argv[3]);
+		ofstream out_file;
+		out_file.open(out_fname, std::ofstream::out | std::ofstream::app);
+		if (!out_file.is_open()) {
+	    	std::cout << "Couldn't open " << out_fname << std::endl;
+		    return -1;
+	    }
+		
+		viewable_angle = 40.0;
+		std::string line;
+		std::getline(data_file, line);
+		char comma;
+		time_t epochtime;
+		
+		/** 
+		 * print kml (head)
+		 */
+		out_file << "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n"
+		         << "<kml xmlns=\"http://www.opengis.net/kml/2.2\"> \n"
+                 << " <Document> \n"
+                 << "  <name>drone-video-play</name> \n"
+                 << "  <Style id=\"transBluePoly\"> \n"
+                 << "   <LineStyle> \n"
+                 << "    <width>1.5</width> \n"
+                 << "   </LineStyle> \n"
+                 << "   <PolyStyle> \n"
+                 << "    <color>7dff0000</color> \n"
+                 << "   </PolyStyle> \n"
+                 << "  </Style> \n";
+		
+		while(!data_file.eof())
+		{
+			std::getline(data_file, line);
+			std::istringstream iss(line);
+		    iss>>lat>>comma>>lng>>comma>>hgt>>comma>>azimuth>>comma>>pitch>>comma>>roll>>comma>>epochtime;
+			hgt = hgt / 1000.0; //meter -> kilometer
+			pitch = pitch + 90.0;
+			roll = 0.0;
+			Drone drone(lat, lng, hgt, azimuth, pitch, roll, viewable_angle); 
+			drone.calc_coverage();
+			
+			/** 
+			 * print kml body. One quadrilateral is corresponding to a placemark
+			 */
+			out_file << "  <Placemark> \n"
+                     << "   <name>Absolute Extruded</name> \n"
+                     << "   <visibility>1</visibility> \n"
+                     << "   <styleUrl>#transBluePoly</styleUrl> \n";
+			
+			struct tm * timeinfo;
+		    timeinfo = localtime (&epochtime);
+			char begin_time[100], end_time[100];
+			std::strftime (begin_time, sizeof(begin_time),"%Y-%m-%dT%H:%M:%S",timeinfo);
+			epochtime = epochtime +1;
+			timeinfo = localtime (&(epochtime));
+			std::strftime (end_time, sizeof(end_time),"%Y-%m-%dT%H:%M:%S",timeinfo);
+			
+			out_file << "   <TimeSpan> \n"
+			         << "   <begin>" << begin_time << "</begin>\n"
+					 << "   <end>" << end_time << "</end>\n"
+					 << "   </TimeSpan>\n"
+					 << "   <MultiGeometry>\n";
+			for(int poly=0; poly<4; poly++)
+			{
+				out_file << "   <Polygon>\n"
+				         << "    <extrude>0</extrude>\n"
+						 << "    <tessellate>1</tessellate>\n"
+						 << "    <altitudeMode>absolute</altitudeMode>\n"
+						 << "    <outerBoundaryIs>\n"
+						 << "     <LinearRing>\n"
+						 << "      <coordinates>\n"
+						 << std::fixed << std::setprecision(6)
+						 << lng << "," << lat << "," << hgt*1000.0 << "\n"
+						 << drone.quadrilateral[2*poly+1] << "," << drone.quadrilateral[2*poly] << ",0\n"         //(1, 0), (3, 2), (5, 4), (7, 6)
+						 << drone.quadrilateral[(2*(poly+1)+1)%8] << "," << drone.quadrilateral[(2*(poly+1))%8] << ",0\n" //(3, 2), (5, 4), (7, 6), (9, 8)
+						 << lng << "," << lat << "," << hgt*1000.0 << "\n"
+						 << lng << "," << lat << "," << hgt*1000.0 << "\n"
+						 << "      </coordinates>\n"
+						 << "     </LinearRing>\n"
+						 << "    </outerBoundaryIs>\n"
+						 << "    </Polygon>\n";
+			}
+			out_file << "   </MultiGeometry>\n"
+			         << "   </Placemark>\n";
+		}
+		/** 
+		 * print kml (tail)
+		 */
+		out_file << " </Document>\n"
+		         << " </kml>\n";
+	}
+	
 	return 0;
 }
 	
